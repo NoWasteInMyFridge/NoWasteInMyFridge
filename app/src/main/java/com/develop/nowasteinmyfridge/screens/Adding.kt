a7e8cf3c2340757fe855a51ab20fc175ac95a4af
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2024-01-10 13:41:31 +0700

    change style

--- app/src/main/java/com/develop/nowasteinmyfridge/screens/Adding.kt
+++ app/src/main/java/com/develop/nowasteinmyfridge/screens/Adding.kt
@@ -10,12 +10,13 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -129,24 +130,31 @@ fun InputFieldWithPlaceholder(
         modifier = Modifier
             .padding(top = 10.dp, bottom = 10.dp)
             .height(30.dp)
-            .background(color = GreenPrimary)
-            .clip(RoundedCornerShape(20.dp)),
-//        contentAlignment = Alignment.Center
+            .background(
+                color = GreenPrimary,
+                shape = RoundedCornerShape(10.dp)
+            )
     ) {
         BasicTextField(
             value = textValue,
             onValueChange = {
                 onValueChange(it)
             },
-            textStyle = MaterialTheme.typography.bodyLarge,
+            textStyle = TextStyle(textAlign = TextAlign.Center),
             modifier = Modifier
                 .fillMaxSize()
+                .padding(horizontal = 16.dp),
+
+
         ) {
             if (textValue.text.isEmpty()) {
                 Text(
                     text = placeholder,
+                    color = White,
+                    fontWeight = FontWeight.SemiBold,
                     style = MaterialTheme.typography.bodyLarge.copy(color = Color.Gray),
-                    modifier = Modifier.padding(start = 4.dp).align(Alignment.CenterStart)
+                    modifier = Modifier.padding(start = 4.dp, top= 4.dp).align(Alignment.CenterStart),
+                            textAlign = TextAlign.Start
                 )
             }
         }
