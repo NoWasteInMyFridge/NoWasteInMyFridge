ab1660e07405635375ce7bcd157b7022a3f80f9d
Pattanan Prarom <78087419+pattanan-pr@users.noreply.github.com>
2024-01-10 14:13:23 +0700

    Setting System - part UI (#5)
    Co-authored-by: pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
    Co-authored-by: jojo <jakarin.chujan@gmail.com>

---
+++ app/src/main/java/com/develop/nowasteinmyfridge/data/model/MenuItem.kt
@@ -0,0 +1,8 @@
+package com.develop.nowasteinmyfridge.data.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuItem(
+    val nameResId: Int,
+    val icon: ImageVector,
+)
