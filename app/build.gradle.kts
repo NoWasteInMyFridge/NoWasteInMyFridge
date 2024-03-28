f28b861bff16bc5e5b815c25984d5dcdcd616773
jojo <jakarin.chujan@gmail.com>
2024-03-28 13:32:52 +0700

    Add dotenv

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,10 +92,12 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
-//    retrofit
+   // retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation ("androidx.compose.runtime:runtime-livedata:1.2.0-alpha03")
 
+    // read env file
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 }
