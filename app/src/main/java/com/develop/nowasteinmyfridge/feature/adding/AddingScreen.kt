bb308309f68f076939203708c806eaf59e6e1ae2
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2024-04-01 16:15:24 +0700

    change img style

--- app/src/main/java/com/develop/nowasteinmyfridge/feature/adding/AddingScreen.kt
+++ app/src/main/java/com/develop/nowasteinmyfridge/feature/adding/AddingScreen.kt
@@ -115,7 +115,6 @@ fun AddingScreen(
         brandsName = brands ?: ""
         scanName = brands?.let { TextFieldValue(it) } ?: TextFieldValue()
         imageUrl = image
-//        selectImageUri = imageUrl
         Log.d("wtf", "$selectImageUri, $brandsName")
     }
 
@@ -147,46 +146,33 @@ fun AddingScreen(
                         .fillMaxWidth()
                         .fillMaxHeight(fraction = 0.35f)
                 ) {
-                    if (selectImageUri != null) {
-                        Image(
-                            painter = rememberAsyncImagePainter(model = selectImageUri),
-                            contentDescription = "",
-                            modifier = Modifier.fillMaxSize(),
-                            contentScale = ContentScale.Crop,
-                        )
-                    } else {
-                        Image(
-                            painter = painterResource(id = R.mipmap.add_photo),
-                            contentDescription = "",
-                            modifier = Modifier.fillMaxSize(),
-                            contentScale = ContentScale.FillBounds
-                        )
-                    }
-                    if (selectImageUri != null) {
-                        Image(
-                            painter = rememberAsyncImagePainter(model = selectImageUri),
-                            contentDescription = "",
-                            modifier = Modifier.fillMaxSize(),
-                            contentScale = ContentScale.Crop,
-                        )
-                    } else {
-                        if (imageUrl != null ) {
-                            AsyncImage(
-                                model = imageUrl,
-                                contentDescription = "Image for $imageUrl",
-                                modifier = Modifier.fillMaxSize(),
-                                contentScale = ContentScale.Fit
-                            )
-                        }
-                        else {
+
+                        if (selectImageUri != null) {
                             Image(
-                                painter = painterResource(id = R.mipmap.add_photo),
+                                painter = rememberAsyncImagePainter(model = selectImageUri),
                                 contentDescription = "",
                                 modifier = Modifier.fillMaxSize(),
-                                contentScale = ContentScale.FillBounds
+                                contentScale = ContentScale.Crop,
                             )
-                        }
+                        } else {
+                            if (imageUrl != null ) {
+                                AsyncImage(
+                                    model = imageUrl,
+                                    contentDescription = "Image for $imageUrl",
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentScale = ContentScale.Crop,
+                                )
+                            }
+                            else {
+                                Image(
+                                    painter = painterResource(id = R.mipmap.add_photo),
+                                    contentDescription = "",
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentScale = ContentScale.FillBounds
+                                )
+                            }
                     }
+
                     Column(
                         modifier = Modifier
                             .wrapContentSize()
